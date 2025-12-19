0851011d765d709300aad66ff72417badf083f98
Krys Miller <krysmiller01@gmail.com>
2025-12-19 15:10:36 -0600

    C: created function in stats that makes dictionary of each character in book in lowercase and counts each into the value of each key

--- stats.py
+++ stats.py
@@ -5,7 +5,22 @@ def count_words(book):
         words = file_contents.split()
         return f"Found {len(words)} total words"
 
+#takes book and counts each character by updating a dictionary
+def count_char(book):
+    with open(book) as f:
+        file_contents = f.read()
+        book_in_lowcase = file_contents.lower()
+        char_in_book = list(book_in_lowcase)
+        #Officially a list of lowercase characters in list
+        char_dict = {}
+        for char in char_in_book:
+            if char in char_dict:
+                char_dict[char] += 1
+            else:
+                char_dict[char] = 1
+        return char_dict
+
 def main():
     print(count_words("books/frankenstein.txt"))
-
+    print(count_char("books/frankenstein.txt"))
 main()
